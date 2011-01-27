4d1663dd3e93abb5ab861ec2152e4086217e3c51
youngtrips <youngtrips@163.com>
2011-01-27 15:44:27 +0800

    update

---
+++ lib/utils.h
@@ -0,0 +1,16 @@
+/*
+ * Author: youngtrips(youngtrips@163.com)
+ * Created Time:  2011-01-27
+ * File Name: utils.h
+ * Description: 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with emesene; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __UTILS_H
+#define __UTILS_H
+
+
+#endif
