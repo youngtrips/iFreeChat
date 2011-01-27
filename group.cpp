776be84c3600385b0c704eb1944564d9643087a9
youngtrips <youngtrips@163.com>
2011-01-27 14:49:22 +0800

    reconstruct

--- group.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Author: youngtrips
- * Created Time:  Tue 25 Jan 2011 05:11:03 PM CST
- * File Name: group.cpp
- * Description: 
- */
-
-
--- makefile
+++
@@ -1,3 +0,0 @@
-libs=`pkg-config --cflags --libs glib-2.0 gtk+-2.0 libglade-2.0`
-all:
-	gcc -o test window.c test.c  $(libs)
